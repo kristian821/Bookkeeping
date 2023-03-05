f443d3dd5834daaa641ad6718cf4fbf646bcd568
Kristian Mitchell <cassiusk@me.com>
2023-03-05 15:03:30 -0800

    Initial Commit

---
+++ Bookkeeping/BookkeepingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookkeepingApp.swift
+//  Bookkeeping
+//
+//  Created by Kristian Mitchell on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct BookkeepingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
