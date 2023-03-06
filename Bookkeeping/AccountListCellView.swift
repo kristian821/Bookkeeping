e433ee272c3b16c1ea1b0f70df5ed6647eed690b
Kristian Mitchell <56667566+kristian821@users.noreply.github.com>
2023-03-06 13:41:35 -0800

    first commit

---
+++ Bookkeeping/AccountListCellView.swift
@@ -0,0 +1,20 @@
+//
+//  AccountListCellView.swift
+//  Bookkeeping
+//
+//  Created by Kristian Mitchell on 3/6/23.
+//
+
+import SwiftUI
+
+struct AccountListCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountListCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountListCellView()
+    }
+}
---
+++ Bookkeeping/AccountListView.swift
@@ -0,0 +1,20 @@
+//
+//  AccountLIstView.swift
+//  Bookkeeping
+//
+//  Created by Kristian Mitchell on 3/6/23.
+//
+
+import SwiftUI
+
+struct AccountLIstView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountLIstView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountLIstView()
+    }
+}
